7f31072e40b1a75d89a835611deba5950cc138f4
Sunjot Singh <sunjot@uoguelph.ca>
2018-02-28 01:00:17 -0500

    fixed up coloring scheme and spacing

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -6,8 +6,9 @@ class Intro extends Component {
       <div id="intro" className={this.props.anim}>
         <div id="intro-cover"></div>
         <div id="intro-desc">
-          <div><p id="role">A <span id="span-dev">developer</span> and <span>des<span id="span-des">i</span>gner</span> based in Toronto.</p></div>
           <div><p id="name">Hi, I'm Sunjot.</p></div>
+          <div><p id="role">A <span id="span-dev">developer</span> and <span>des<span id="span-des">i</span>gner</span> based in Toronto.</p></div>
+          <div><p id="line"></p></div>
         </div>
       </div>
     );
