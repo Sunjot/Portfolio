63d2edd0894e8d2a974d4f28361fa86a06148325
Sunjot Singh <sunjot@uoguelph.ca>
2018-02-28 04:42:18 -0500

    added template for projects comp

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 class Projects extends Component {
   render() {
     return (
-      <div id="projects">
-        <p>Test</p>
+      <div id="projects-cont">
+        <div id="projects">
+          <p className="heading">My Work</p>
+          <div id="act-div" className="divider"/>
+        </div>
       </div>
     );
   }
