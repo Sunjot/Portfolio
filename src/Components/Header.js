18edde9a3895ad9076f7361f7cf4059e53f5c3d7
Sunjot Singh <sunjot@uoguelph.ca>
2018-02-28 03:09:07 -0500

    implemented some diff animation for header links

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,14 +3,32 @@ import { Link } from 'react-router-dom';
 
 class Header extends Component {
 
+  constructor() {
+    super();
+    this.
+
+    this.state = {
+      bActive: ""
+    }
+  }
+
+
   render() {
     return (
       <div id="header">
         <div id="header-link-collect">
-          <Link to="/" id="hl1" className="header-link" key="hl1">Home</Link>
-          <Link to="/me" id="hl2" className="header-link" key="hl1">About Me</Link>
-          <Link to="/projects" id="hl3" className="header-link" key="hl1">Projects</Link>
-          <Link to="/contact" id="hl4" className="header-link" key="hl1">Get in Touch</Link>
+          <div id="hl1">
+            <Link to="/" id="hl1-a" className="header-link" key="hl1">Home</Link>
+          </div>
+          <div id="hl2" onClick={() => this.}>
+            <Link to="/me" id="hl2-a" className="header-link" key="hl1">About Me</Link>
+          </div>
+          <div id="hl3">
+            <Link to="/projects" id="hl3-a" className="header-link" key="hl1">Projects</Link>
+          </div>
+          <div id="hl4">
+            <Link to="/contact" id="hl4-a" className="header-link" key="hl1">Get in Touch</Link>
+          </div>
         </div>
         <div id="header-line"/>
       </div>
