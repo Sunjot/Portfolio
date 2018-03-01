27e65a8550754dac1801ed6970d535759f323965
Sunjot Singh <sunjot@uoguelph.ca>
2018-03-01 02:31:08 -0500

    added tvtracker image/desc to projects comp

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -8,11 +8,7 @@ class Intro extends Component {
       <div id="intro" className={this.props.anim}>
         <div id="intro-cover"></div>
         <div id="intro-desc">
-          <div id="name">
-            <ReactSVG
-                path={Name}
-              />
-          </div>
+          <div id="name"> <ReactSVG path={Name}/></div>
           <div><p id="role">A <span id="span-dev">front-end developer</span> based in Toronto.</p></div>
           <div><p id="line">I build clean and responsive websites with minimalist design in mind.</p></div>
         </div>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Tv from '../Images/tv.jpg';
 
 class Projects extends Component {
   render() {
@@ -7,6 +8,15 @@ class Projects extends Component {
         <div id="projects">
           <p className="heading">My Work</p>
           <div id="act-div" className="divider"/>
+          <div id="tv-cont">
+            <img id="tv-pic" src={Tv}/>
+            <div id="tv-desc">
+              <p className="title">TV Tracker</p>
+              <p className="desc">TV Tracker allows you to create a collection of all your past- and
+              presently-watched TV shows in order to keep track of everything, including what's
+              airing in the coming week. Built using Ruby on Rails and powered by the The Movie DB.</p>
+            </div>
+          </div>
         </div>
       </div>
     );
