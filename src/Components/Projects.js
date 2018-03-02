f615a215683734c9a45a82cfdd3339354f8f3ef6
Sunjot Singh <sunjot@uoguelph.ca>
2018-03-01 21:22:07 -0500

    added second proj

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Tv from '../Images/tv.jpg';
+import Got from '../Images/therealm.jpg';
 
 class Projects extends Component {
   render() {
@@ -8,13 +9,25 @@ class Projects extends Component {
         <div id="projects">
           <p className="heading">My Work</p>
           <div id="act-div" className="divider"/>
-          <div id="tv-cont">
-            <img id="tv-pic" src={Tv}/>
-            <div id="tv-desc">
+
+          <div id="tv-cont" className="proj-cont">
+            <img id="tv-pic" className="proj-pic" src={Tv}/>
+            <div id="tv-desc" className="desc-cont">
               <p className="title">TV Tracker</p>
               <p className="desc">TV Tracker allows you to create a collection of all your past- and
-              presently-watched TV shows in order to keep track of everything, including what's
-              airing in the coming week. Built using Ruby on Rails and powered by the The Movie DB.</p>
+              presently-watched TV shows in order to keep track of what's
+              airing in the coming week and more. Built using Ruby on Rails and powered by the
+              The Movie DB.</p>
+            </div>
+          </div>
+          <div id="got-cont" className="proj-cont">
+            <img id="got-pic" className="proj-pic" src={Got}/>
+            <div id="got-desc" className="desc-cont">
+              <p className="title">The Realm</p>
+              <p className="desc">TV Tracker allows you to create a collection of all your past- and
+              presently-watched TV shows in order to keep track of what's
+              airing in the coming week and more. Built using Ruby on Rails and powered by the
+              The Movie DB.</p>
             </div>
           </div>
         </div>
