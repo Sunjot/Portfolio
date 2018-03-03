32f3188a7140d06e1544ace6bfb7b88c35d460bf
Sunjot Singh <sunjot@uoguelph.ca>
2018-03-02 23:30:49 -0500

    added feather icons, finished tv tracker sec w/ animation

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Tv from '../Images/tv.jpg';
 import Got from '../Images/therealm.jpg';
+import { ExternalLink } from 'react-feather';
+import { ArrowLeftCircle } from 'react-feather';
 
 class Projects extends Component {
 
@@ -8,16 +10,18 @@ class Projects extends Component {
     super();
     this.state = {
       selectedProj: "",
-      tvAnim: "proj-in-left",
-      gotAnim: "proj-in-right"
+      tvAnim: "tv-in",
+      gotAnim: "got-in",
+      projAnim: "proj-in"
     }
   }
 
   projClick = (e) => {
 
     this.setState({
-      tvAnim: "proj-out-left",
-      gotAnim: "proj-out-right"
+      tvAnim: "tv-out",
+      gotAnim: "got-out",
+      projAnim: "proj-in"
     })
 
     if (e === "tv") {
@@ -36,6 +40,20 @@ class Projects extends Component {
     }
   }
 
+  backClick = () => {
+    this.setState({
+      projAnim: "proj-out"
+    });
+
+    setTimeout(() => {
+      this.setState({
+        selectedProj: "",
+        tvAnim: "tv-in",
+        gotAnim: "got-in"
+      });
+    }, 500);
+  }
+
   render() {
     return (
       <div id="projects-cont">
@@ -44,20 +62,28 @@ class Projects extends Component {
           <div id="act-div" className="divider"/>
           {this.state.selectedProj === "" &&
             <div id="pic-list">
-              <img className={this.state.tvAnim} src={Tv} onClick={() => this.projClick("tv")}/>
-              <img className={this.state.gotAnim} src={Got} onClick={() => this.projClick("got")}/>
+              <img className={this.state.tvAnim} src={Tv} onClick={() => this.projClick("tv")} alt="tv"/>
+              <img className={this.state.gotAnim} src={Got} onClick={() => this.projClick("got")} alt="got"/>
             </div>
           }
           {this.state.selectedProj === "tv" &&
-            <div className="proj-cont">
-              <p className="heading">Tv Tracker</p>
-              <p className="desc">A website that allows you to build a collection of your past and presently watched
-              TV shows. Discover new series based on what you've seen already and keep tabs on
-              what is scheduled to be on-air in the coming week.</p>
+            <div id={this.state.projAnim} className="proj-cont">
+              <div className="heading-cont">
+                <p className="heading">Tv Tracker</p>
+                <a target="_blank" rel="noopener noreferrer" href="https://tv.imalwayssunny.xyz">
+                  <ExternalLink className="feather-icon" color="white" size={28} />
+                </a>
+              </div>
+              <p className="desc">A website that allows you to build a collection of your past
+                and presently watched TV shows. Discover new series based on what you've seen already
+                and keep tabs on what is scheduled to be on-air in the coming week. Built using Ruby
+                on Rails and powered by The Movie DB.
+              </p>
+              <ArrowLeftCircle onClick={() => this.backClick()} id="arrow-icon" className="feather-icon" color="white" size={35} />
             </div>
           }
           {this.state.selectedProj === "got" &&
-            <div className="proj-cont">
+            <div id={this.state.projAnim} className="proj-cont">
               <p className="heading">The Realm</p>
               <p className="desc"></p>
             </div>
