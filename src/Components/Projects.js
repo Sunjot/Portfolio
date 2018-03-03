4112fcc13460838507a247eda7fb11d5f61c7e57
Sunjot Singh <sunjot@uoguelph.ca>
2018-03-02 22:20:01 -0500

    slide in/out anim in projs and some content added

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,33 +3,65 @@ import Tv from '../Images/tv.jpg';
 import Got from '../Images/therealm.jpg';
 
 class Projects extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      selectedProj: "",
+      tvAnim: "proj-in-left",
+      gotAnim: "proj-in-right"
+    }
+  }
+
+  projClick = (e) => {
+
+    this.setState({
+      tvAnim: "proj-out-left",
+      gotAnim: "proj-out-right"
+    })
+
+    if (e === "tv") {
+      setTimeout(() => {
+        this.setState({
+          selectedProj: "tv"
+        });
+      }, 1000);
+    }
+    if (e === "got") {
+      setTimeout(() => {
+        this.setState({
+          selectedProj: "got"
+        });
+      }, 1000);
+    }
+  }
+
   render() {
     return (
       <div id="projects-cont">
         <div id="projects">
           <p className="heading">My Work</p>
           <div id="act-div" className="divider"/>
-
-          <div id="tv-cont" className="proj-cont">
-            <img id="tv-pic" className="proj-pic" src={Tv}/>
-            <div id="tv-desc" className="desc-cont">
-              <p className="title">TV Tracker</p>
-              <p className="desc">TV Tracker allows you to create a collection of all your past- and
-              presently-watched TV shows in order to keep track of what's
-              airing in the coming week and more. Built using Ruby on Rails and powered by the
-              The Movie DB.</p>
+          {this.state.selectedProj === "" &&
+            <div id="pic-list">
+              <img className={this.state.tvAnim} src={Tv} onClick={() => this.projClick("tv")}/>
+              <img className={this.state.gotAnim} src={Got} onClick={() => this.projClick("got")}/>
             </div>
-          </div>
-          <div id="got-cont" className="proj-cont">
-            <img id="got-pic" className="proj-pic" src={Got}/>
-            <div id="got-desc" className="desc-cont">
-              <p className="title">The Realm</p>
-              <p className="desc">TV Tracker allows you to create a collection of all your past- and
-              presently-watched TV shows in order to keep track of what's
-              airing in the coming week and more. Built using Ruby on Rails and powered by the
-              The Movie DB.</p>
+          }
+          {this.state.selectedProj === "tv" &&
+            <div className="proj-cont">
+              <p className="heading">Tv Tracker</p>
+              <p className="desc">A website that allows you to build a collection of your past and presently watched
+              TV shows. Discover new series based on what you've seen already and keep tabs on
+              what is scheduled to be on-air in the coming week.</p>
             </div>
-          </div>
+          }
+          {this.state.selectedProj === "got" &&
+            <div className="proj-cont">
+              <p className="heading">The Realm</p>
+              <p className="desc"></p>
+            </div>
+          }
         </div>
       </div>
     );
