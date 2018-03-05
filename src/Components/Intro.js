469d1fdba5f5c92351b1c89f99f444501a448ac1
Sunjot Singh <sunjot@uoguelph.ca>
2018-03-05 04:05:34 -0500

    added social links to homepage

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ReactSVG from 'react-svg';
 import Name from '../Images/name.svg';
+import Github from '../Images/icons_github.svg';
+import Linkedin from '../Images/icons_linkedin.svg';
 
 class Intro extends Component {
   render() {
@@ -8,8 +10,11 @@ class Intro extends Component {
       <div id="intro" className={this.props.anim}>
         <div id="intro-desc">
           <div id="name"> <ReactSVG path={Name}/></div>
-          <div><p id="role">A <span id="span-dev">front-end developer</span> based in Toronto.</p></div>
-          <div><p id="line">I build clean and responsive websites with minimalist design in mind.</p></div>
+          <div><p id="role">A <span id="span-dev">web developer</span> based in Toronto.</p></div>
+        </div>
+        <div id="social-links">
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/Sunjot"><ReactSVG path={Github} /></a>
+          <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/sunjotsingh/"><ReactSVG path={Linkedin} /></a>
         </div>
       </div>
     );
